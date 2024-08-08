b721c42b8cb4cbd31ae4d3db86c0811ddd6eb34a
Xiuyuan <u7678992@anu.edu.au>
2024-08-08 22:25:01 +1000

    Initialised front end

---
+++ client/pages/index.tsx
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Spacer, Card, CardBody, CardFooter} from "@nextui-org/react";
+import Image from 'next/image'
+
+function index() {
+  const [message, setMessage] = useState("Loading");
+  const [list, setList] = useState([]);
+  const logo = require('../images/logo.png');
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/home")
+      .then((response) => response.json())
+      .then((data) => {
+        setMessage(data.test);
+        setList(data.testList);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Navbar isBordered>
+      <NavbarBrand>
+        <Image
+          src={logo}
+          alt="Logo"
+          height="60"
+        />
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            About Us
+          </Link>
+        </NavbarItem>
+        <Spacer x={6} />
+        <NavbarItem>
+          <Link href="search" color="foreground" >
+            Services
+          </Link>
+        </NavbarItem>
+        <Spacer x={6} />
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Tools
+          </Link>
+        </NavbarItem>
+        <Spacer x={6} />
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            People
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+      <div>{message}</div>
+
+      {list.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
+  );
+}
+
+export default index;
