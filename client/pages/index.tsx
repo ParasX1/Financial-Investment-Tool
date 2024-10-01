207ccc295c7d8a93323dff0ceaab877fe957a78f
Xiuyuan <u7678992@anu.edu.au>
2024-10-01 14:50:43 +1000

    Added graph components

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -19,8 +19,10 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { Button as MUIButton } from '@mui/material';
 import ModalSignUp from "@/components/Modal/ModalSignUp";
+import BarGraph from "@/components/bargraph";
 import Sidebar from "@/components/sidebar"; // Adjust the path to match where Sidebar is located in your project
 import 'boxicons/css/boxicons.min.css';
+import LineGraph from "@/components/linegraph";
 
 
 function TextGrid() {
@@ -31,7 +33,6 @@ function TextGrid() {
   const img4 = require("@/assets/gridBackground1.png");
   const img5 = require("@/assets/gridBackground1.png");
   const img6 = require("@/assets/gridBackground1.png");
-
   const items = [
     { text: (
           <Box>
@@ -145,7 +146,21 @@ function Index() {
   const img1 = require("@/assets/gridBackground1.png");
   const imgStar = require("@/assets/star.png");
   const team = require("@/assets/team.png");
-
+  const exampleData = [
+    { label: 'Apples', value: 50 },
+    { label: 'Bananas', value: 80 },
+    { label: 'Cherries', value: 30 },
+    { label: 'Dates', value: 70 },
+    { label: 'Elderberries', value: 45 },
+  ];
+  const timeSeriesData = [
+    { date: new Date('2024-01-01'), value: 50 },
+    { date: new Date('2024-01-02'), value: 55 },
+    { date: new Date('2024-01-03'), value: 60 },
+    { date: new Date('2024-01-04'), value: 40 },
+    { date: new Date('2024-01-05'), value: 75 },
+    { date: new Date('2024-01-06'), value: 85 },
+  ];
   // Signup-Login Modal
   const [showSignUp, setSignUp] = useState(false);
   const [showLogIn, setShowLogIn] =useState(false);
@@ -155,14 +170,14 @@ function Index() {
   const handleSignUpShow = () => setSignUp(true);
   const handleSignUpClose = () => setSignUp(false);
 
-  useEffect(() => {
-    fetch("http://localhost:8080/api/home")
-      .then((response) => response.json())
-      .then((data) => {
-        setMessage(data.test);
-        setList(data.testList);
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:8080/api/home")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setMessage(data.test);
+  //       setList(data.testList);
+  //     });
+  // }, []);
 
 
   return (
@@ -223,12 +238,8 @@ function Index() {
               </NavbarItem>
             </NavbarContent>
           </Navbar>
-          <div>{message}</div>
-
-          {list.map((item, index) => (
-            <div key={index}>{item}</div>
-          ))}
-
+          <BarGraph data={exampleData} width={600} height={400}/>
+          <LineGraph data={timeSeriesData} width={600} height={400} />,
           <div className="two-column">
             <div className="left-column">
               <h1 className="title-text">FIT</h1>
