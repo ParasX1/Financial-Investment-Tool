29aa28db586823cceeca58d30df13ddc4dc94849
jiahengguo <guojiaheng123@gmail.com>
2025-05-17 20:19:33 +1000

    initialised market news blocks

--- client/pages/MarketNews.tsx
+++ client/pages/MarketNews.tsx
@@ -1,17 +1,53 @@
 import React from "react";
 import Sidebar from "@/components/sidebar"; // Adjust the path to match where Sidebar is located in your project
 
-function MarketNews() {
-  return (
-    <div style={{ display: "flex" }}>
-      <Sidebar />
-      <div style={{ flex: 1, paddingLeft: "50px" }}>
-        {/* Your portfolio page content will go here */}
-        <h1>Market News</h1>
-        <p>This is where you can add your market news content.</p>
-      </div>
-    </div>
-  );
-}
+import { Grid} from '@mui/material';
+
+
+const MarketNews: React.FC = () => {
+    
+
+    return (
+        <div>
+            <div style={{ display: 'flex' }}>
+                <Sidebar />
+            
+
+            
+                    <div style={{ padding: '20px' }}>
+                        <Grid container spacing={2}>
+                            {/* Main Large Card */}
+                            <Grid item xs={12} md={8}>
+                                
+                            </Grid>
+
+                            {/* Vertical Stack of Cards */}
+                            <Grid item xs={12} md={4}>
+                                <Grid container direction="column" spacing={2}>
+                                    {[1, 2].map((index) => (
+                                        <Grid item key={index}>
+                                            
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                            </Grid>
+
+                            {/* Bottom Row of Cards */}
+                            <Grid item xs={12}>
+                                <Grid container spacing={2}>
+                                    {[3, 4, 5].map((index) => (
+                                        <Grid item xs={12} sm={4} key={index}>
+                                            
+                                        </Grid>
+                                    ))}
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </div>
+            </div>
+        </div>
+    );
+};
 
 export default MarketNews;
+
