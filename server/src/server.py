45c1d30e395068c6ec3d877b9dcab8f0f4375ad2
LachlanMajor <majorlachie@gmail.com>
2025-07-16 19:28:11 +1000

    fixed import for working ci

--- server/src/server.py
+++ server/src/server.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 from src.stocks import sanitiseStockJson
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-from metrics import (
+from src.metrics import (
     fetch_stock_data,
     calculate_beta,
     calculate_alpha,
