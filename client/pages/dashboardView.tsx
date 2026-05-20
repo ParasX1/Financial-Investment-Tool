9ba13769e0dd1e18b9f6df1e677f66af4faee090
JunyuLuo123 <u7110253@anu.edu.au>
2026-05-20 16:43:02 +1000

    fix

--- client/pages/dashboardView.tsx
+++ client/pages/dashboardView.tsx
@@ -364,7 +364,7 @@ const DashboardView: React.FC = () => {
                                 alignItems: 'flex-end',
                                 gap: 'clamp(10px, 1vw, 16px)',
                                 flexWrap: { xs: 'wrap', lg: 'nowrap' },
-                                mt: 7,
+                                mt: 0,
                                 minHeight: 0,
                             }}
                         >
